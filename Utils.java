91d273b0e7da8e818ca85c8f55aa93c77564cc2f
Habbit-Tracker <eh.vandaki@gmail.com>
2024-04-24 12:58:28 +0330

    is email valid

---
+++ Utils.java
@@ -0,0 +1,14 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Utils {
+
+
+
+    public static boolean isEmailValid(String email){
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
